94e8b8dbd75fd39c615264efea2a1cc396c6079c
duxinejie <duxinjie88@sina.com>
2017-02-10 00:14:04 +0800

    leetcoding

---
+++ arrangingcoins.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+class Solution(object):
+    def __init__(self):
+	self.result = 0
+    def arrangeCoins(self,n):
+	if n < 0:
+	    return False
+	a = 0
+	while True:
+	    if 2*n < a*(a + 1):
+		break
+	    a = a + 1
+	return a-1
+	
+s = Solution()
+print s.arrangeCoins(1)
